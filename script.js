b69ab0042b85992a4f4c12677d46f5d0b3b55c36
CadetheCoder <cadehultz06@gmail.com>
2025-02-01 23:19:51 +0900

    Add event button

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-let hello = "world"
+const introButton = document.getElementById("intro-button");
+const header = document.getElementById("header");
+const note = document.getElementById("note");
 
-console.log(hello)
+const changeText = () => {
+    note.innerText = "I love you"
+    header.innerText = "Happy Birthday!"
+    introButton.style.display = "none"
+}
+
+introButton.addEventListener("click", changeText)
+
