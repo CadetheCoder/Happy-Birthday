c3fb49f89f9955c7104d6c23662e98212f8bb376
CadetheCoder <cadehultz06@gmail.com>
2025-02-09 13:11:21 +0900

    Added more quotes to the array

--- secondscript.js
+++ secondscript.js
@@ -1,4 +1,4 @@
-const array = ["She's an eepy baby 😴", "She's supa smart 🤓", "She has the most beautiful voice 🕊️", "She's the prettiest girl in the whole world 😍", "Shes way older than her boyfriend 😏", "She’s Sudanese 🇮🇩", "She’s 22 🥰", "She loves spicy food 🥵", "Her favorite animal is cats 🐈‍"];
+const array = ["She's an eepy baby 😴", "She's supa smart 🤓", "She has the most beautiful voice 🕊️", "She's the prettiest girl in the whole world 😍", "She's way older than her boyfriend 😏", "She’s Sudanese 🇮🇩", "She’s 22 🥰", "She loves spicy food 🥵", "Her favorite animal is cats 🐈‍", "Dia sangat cantik 💕",  "彼女は本当に美しい ❤️‍🔥"];
 const factsButton = document.getElementById("facts-button");
 const funFacts = document.getElementById("fun-facts");
 
